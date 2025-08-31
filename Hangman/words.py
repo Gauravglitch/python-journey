d566a7c536e77ec721330c8ef8000108a6ad0a5f
Gaurav <gauravmaharjan1@gmail.com>
2025-08-31 23:03:17 +0545

    Made seperate list of words file

---
+++ Hangman/words.py
@@ -0,0 +1,4 @@
+words =("apple","banana","grape","orange","strawberry","watermelon","kiwi",
+        "mango","peach","pear","plum","cherry","blueberry","raspberry","blackberry",
+        "coconut","papaya","pomegranate","apricot","fig","date","melon","tangerine",
+        "nectarine","clementine","currant","jackfruit","durian")
