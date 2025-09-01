16c0d06b28b359262bcf2426fcf7de69cca16d55
Gaurav <gauravmaharjan1@gmail.com>
2025-09-02 01:44:53 +0545

    Added list feature for number list display

--- 21 Number Game/main.py
+++ 21 Number Game/main.py
@@ -1,28 +1,43 @@
 # 21 Number Game
 import random
 
-last_number = 0 #game starts before "1"
+last_num = 0 #game starts before "1"
+nums = [] #will store the full sequence
+
 
 while True:
     #Players' turns
     move = int(input("How many numbers will you say? (1-3): "))
 
-    #update last_number
-    last_number += move
+    for i in range(1,move+1):
+        next_num = last_num + 1
+        last_num = next_num
+        nums.append(next_num)
 
-    print("You said:", last_number)
+        if next_num == 21:
+            print("You said:", nums[-move:]) #show only the last numbers said
+            print("Numbers so far:", nums) #show all numbers said
+            print("You reached 21! You lose!")
+            exit()
 
-    if last_number >= 21:
-        print("You reached 21! You lose!")
-        break
+    print("You said:", nums[-move:]) #show only the last numbers said
+    print("Numbers so far:", nums) #show all numbers said
 
     #Computer's turn
     comp_move = random.randint(1, 3)
-    last_number += comp_move
-    print("Computer says:", last_number)
 
-    if last_number >= 21:
-        print("Computer reached 21! You win!")
-        break
+    for i in range(1,comp_move+1):
+        next_num = last_num + 1
+        last_num = next_num 
+        nums.append(next_num)
+
+        if next_num == 21:
+            print("Computer says:", nums[-comp_move:]) #show only the last numbers said
+            print("Numbers so far:", nums) #show all numbers said
+            print("Computer reached 21! You win!")
+            exit()
+
+    print("Computer says:", nums[-comp_move:]) #show only the last numbers said
+    print("Numbers so far:", nums) #show all numbers said
 
 
